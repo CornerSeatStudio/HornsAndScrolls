469bd01efdeb1a2593bac2d550f22dd530eaefd2
water0300 <yuen.nathan@yahoo.com>
2020-07-03 21:54:52 -0700

    fixed enum removal for eawpontiype

--- Assets/Scripts/ScriptableObjects/WeaponData.cs
+++ Assets/Scripts/ScriptableObjects/WeaponData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class WeaponData : ScriptableObject
 {
     public string WeaponName;
-    public WeaponType weaponType;
+    //public WeaponType weaponType;
     public float startup;
     public float endlag;
     public float damage;
