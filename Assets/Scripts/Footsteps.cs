ad64bc3b235df6e18f747c51c0584cf3d01791f7
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2020-07-08 13:21:40 +0800

    Audio Scripts

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+    private void step(){
+        AudioClip clip= GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+    private AudioClip GetRandomAudio(){
+        return clips[UnityEngine.Random.Range(0,clips.Length)];
+    }
+}
