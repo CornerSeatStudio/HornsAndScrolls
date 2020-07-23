9da60097feecc42021df19c2c2620e1848810fac
water0300 <yuen.nathan@yahoo.com>
2020-07-22 22:26:25 -0700

    Revert "Merge branch 'master-unfucked' into ai-ik-refactor"
    This reverts commit b0c4543e0de7593f85949b0eb7b25256bcd2fccd, reversing
    changes made to b348aab9744a4d5e254dacee94e077680166ba3c.

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -16,14 +16,7 @@ public class CameraHandler : MonoBehaviour {
 
     void Start(){
         cam = this.GetComponent<Camera>();
-
         offset = new Vector3(0, 30f, -25f);
-
-        //default settings
-     //   cam.fieldOfView = fov;
-        
-
-
     }
 
     //general rule: read data in update, handle data in fixed update
@@ -40,30 +33,10 @@ public class CameraHandler : MonoBehaviour {
         //scroll for camera movement (within bounds)
         // //probably should deal with jitter - possibly use a lerp
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
-
         // if(offset.sqrMagnitude >= minOffset.sqrMagnitude && offset.sqrMagnitude <= maxOffset.sqrMagnitude){
         //     offset.y += scrollInput;
         //     offset.z += scrollInput;
         // } 
-
-        //if(cam.fieldOfView >= fovLowerBound && cam.fieldOfView <= fovUpperBound){
-            //cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, cam.fieldOfView - (scrollInput * scrollZoomMultiplier), scrollZoomSmoothness);
-       // } else if (cam.fieldOfView < fovLowerBound) {
-         //   cam.fieldOfView = fovLowerBound;
-     //   } else if (cam.fieldOfView > fovUpperBound) {
-        //    cam.fieldOfView = fovUpperBound;
-      //  }
-        if(cam.fieldOfView < fovLowerBound){
-            cam.fieldOfView= fovLowerBound;
-        }else if(cam.fieldOfView >= fovLowerBound && cam.fieldOfView <= fovUpperBound){
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, cam.fieldOfView - (scrollInput * scrollZoomMultiplier), scrollZoomSmoothness);
-        }
-        if(cam.fieldOfView > fovUpperBound) {
-            cam.fieldOfView = fovUpperBound;
-        }else if(cam.fieldOfView >= fovLowerBound && cam.fieldOfView <= fovUpperBound){
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, cam.fieldOfView - (scrollInput * scrollZoomMultiplier), scrollZoomSmoothness);
-        }
-
 
     }
 
