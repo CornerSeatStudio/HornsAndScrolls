f24dbbd43b0dd771d460b8af7183a5df898f7946
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2020-08-21 13:26:47 +0800

    save abd load

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
 {
+    
+
     public void NextScene(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+
+    public void PreviousScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void quit(){
+        Application.Quit();
+    }
+    public void load(string SceneName){
+        SceneManager.LoadScene(SceneName);
+    }
+
 }
