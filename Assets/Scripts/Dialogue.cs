8e75a74bd9d7f1078ff273a37918a4f066300da5
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2020-08-23 12:53:35 +0800

    dialogue system

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (dialoguedictionary dialogue){
+        Debug.Log("Starting");
+    }
+}
