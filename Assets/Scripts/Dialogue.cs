b7481a2cc989a1663ad96a1ae33fdae574eaf8c7
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2020-08-24 12:52:33 +0800

    dialogue

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Dialogue : MonoBehaviour
 {
     private Queue<string> sentences;
 
-
-
+    public Text dialogueText;
+    public Text nameText;
     void Start()
     {
         sentences = new Queue<string>();
     }
 
     public void StartDialogue (dialoguedictionary dialogue){
-        Debug.Log("Starting");
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence(){
+        if(sentences.Count ==0){
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+    void EndDialogue(){
+
     }
 }
