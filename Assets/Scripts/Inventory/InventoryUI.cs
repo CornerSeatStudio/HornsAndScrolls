767b5c8a36d71d5cbc9c7f2aad115482db64f408
water0300 <yuen.nathan@yahoo.com>
2020-09-05 22:10:03 -0700

    detec wing

---
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventoryUI : MonoBehaviour
+{
+    InventoryObject playerInventory;
+
+    TextMeshProUGUI healthPotCount;
+    TextMeshProUGUI staminaPotCount;
+
+    public void Start(){
+
+        OnInventoryUpdate();
+    }
+
+    public void OnInventoryUpdate(){
+        healthPotCount.SetText(playerInventory.FindInInventory("HealthPot").quantity.ToString());
+        staminaPotCount.SetText(playerInventory.FindInInventory("StaminaPot").quantity.ToString());
+    }
+}
